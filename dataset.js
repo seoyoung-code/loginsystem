fa18cb9fb6835f932e56b6ad13ca0e454ab6b522
itg <itg@DESKTOP-89GP87V>
2025-01-27 12:40:15 +0900

    2

--- dataset.js
+++ dataset.js
@@ -58,7 +58,7 @@ d2.set_img(0,{imgtitle:"분리된 공통 모듈 테스트 페이지",imgurl:"htt
 d2.set_img(0,{imgtitle:"공통모듙 테스트 페이지 로그인 성공 ",imgurl:"https://drive.google.com/file/d/12DbJfXcurAIXROsQy3IC3BnfxdqMl-Vj/view?usp=drive_link",imglog:"분리된 모듈 페이지의 로그인이 성공하였습니다.",sourceurl:""})//이미지타이틀
 
 d2.set_content("테스트케이스를 작성합니다.")
-d2.set_img(1,{imgtitle:"경계값 분석 테스트케이스",imgurl:"https://drive.google.com/file/d/1Qq7TnoAbpDZQ91TU45XuTRLO1LaNDUDI/view?usp=drive_link",imglog:"경계값 분석 후 테스트케이스 작성",sourceurl:"https://github.com/seoyoung-code/loginsystem/blob/main/LoginSystem/src/test/java/com/itg/examp/test/MemberServiceTests.java"})//이미지타이틀
+d2.set_img(1,{imgtitle:"경계값 분석 테스트케이스",imgurl:"https://drive.google.com/file/d/1cTGbJZT_RMlg6mXn6564U2UwI6FX3Rpr/view?usp=drive_link",imglog:"경계값 분석 후 테스트케이스 작성",sourceurl:"https://github.com/seoyoung-code/loginsystem/blob/main/LoginSystem/src/test/java/com/itg/examp/test/MemberServiceTests.java"})//이미지타이틀
 
 d2.set_content("단위테스트를 작성합니다.")
 d2.set_img(2,{imgtitle:"단위테스트",imgurl:"https://drive.google.com/file/d/1ODg3i-Aa8TamQrNQGg8u4cXn8IqgkKWA/view?usp=drive_link",imglog:"아이디, 비밀번호 관련 단위테스트 작성",sourceurl:"https://docs.google.com/document/d/1yE5rKn2qTPMFFR9JNNGTpyL7Yta2BgvMvhabtO9vJ4E/edit?usp=drive_link"})//이미지타이틀
@@ -66,7 +66,7 @@ d2.set_img(2,{imgtitle:"실행",imgurl:"https://drive.google.com/file/d/1K0SZmZy
 d2.set_fill("사용자와 프로그램간에 인터렉티브한 로그인 세션 설정")//사용자 에필로그
 
 d2.set_content("테스트결과보고서를 작성합니다.")
-d2.set_img(3,{imgtitle:"경계값 분석 테스트케이스",imgurl:"https://drive.google.com/file/d/1Qq7TnoAbpDZQ91TU45XuTRLO1LaNDUDI/view?usp=drive_link",imglog:"경계값 분석 후 테스트케이스 작성",sourceurl:"https://github.com/seoyoung-code/loginsystem/blob/main/LoginSystem/src/test/java/com/itg/examp/test/MemberServiceTests.java"})//이미지타이틀
+d2.set_img(3,{imgtitle:"테스트 결과보고서",imgurl:"https://drive.google.com/file/d/1X181OBtxl07juPTyC1Tuv3-9HF6jTugw/view?usp=drive_link",imglog:"경계값 분석 후 테스트 결과 보고서 작성",sourceurl:"https://docs.google.com/document/d/1WA2At0jw21OfGBkxBGT4czKLVIYL0O5iAMXNM29AqOQ/edit?tab=t.0"})//이미지타이틀
 data_sets.push(d2)
 
 // menu3 =============================================================
